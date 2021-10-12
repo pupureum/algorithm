ee7dbdf04134448295727bdf499a38e61f06ed0b
Pureum Lee <plee@c10r3s2.42seoul.kr>
2021-10-12 16:19:09 +0900

    :)

---
+++ 10809/10809.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(void)
+{
+    string str;
+    string alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+    cin >> str;
+    for (int i = 0; i < alphabet.length(); i++)
+        cout << (int)str.find(alphabet[i]) << " ";
+    cout << endl; 
+    return (0);
+}
---
+++ 10809/10809.py
@@ -0,0 +1,5 @@
+str = input()
+alphabet = 'abcdefghijklmnopqrstuvwxyg'
+
+for i in alphabet:
+    print((str.find(i)), end = " ")

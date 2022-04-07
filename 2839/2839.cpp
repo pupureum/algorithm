a5eaa0932aa9c3895e3585343019c6d6750b059c
pureum <dlvnfma0501@naver.com>
2022-04-07 22:27:09 +0900

    :)

---
+++ 2839/2839.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	int N, cnt = 0;
+	cin >> N;
+
+	while(N > 0)
+	{
+		if (N % 5 == 0)
+		{
+			N -= 5;
+			cnt++;
+		}
+		else if (N % 3 == 0)
+		{
+			N -= 3;
+			cnt++;
+		}
+		else if (N > 5)
+		{
+			N -= 5;
+			cnt++;
+		}
+		else
+		{
+			cnt = -1;
+			break;
+		}
+	}
+	cout << cnt << endl;
+	return (0);
+}

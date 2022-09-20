9a27b17649b25f89134f853cf152bca0a9b74c50
pupureum <dlvnfma0501@naver.com>
2022-09-21 02:45:52 +0900

    [FEAT] check 제거

--- programmers/level2/올바른 괄호/solution.py
+++ programmers/level2/올바른 괄호/solution.py
@@ -7,16 +7,13 @@
 def solution(s):
     answer = True
     stack = []
-    check = 0
     for i in range(len(s)):
         if s[0] == ")" or (len(stack) == 0 and s[i] == ")"):
             return False
         if s[i] == "(":
-            check += 1
             stack.append(s[i])
         else:
             stack.pop()
-            check -= 1
-    if check > 0:
+    if len(stack) != 0:
         answer = False
     return answer

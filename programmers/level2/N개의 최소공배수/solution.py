41e502fe18cdeaf7dbc22706ce95e41b33166463
pupureum <pureuml.dev@gmail.com>
2023-03-03 15:29:22 +0900

    refactor: 로직 개선

--- programmers/level2/N개의 최소공배수/solution.py
+++ programmers/level2/N개의 최소공배수/solution.py
@@ -8,3 +8,13 @@ def solution(arr):
             if (i == len(arr) - 2):
                 return answer
 print(solution([2,6,8,14]))
+
+# 개선한 코드!
+# a와 b의 최소공배수를 x라고 하자, x = (a * b) / a, b의 최대공약수
+from math import gcd
+
+def advancedSolution(arr):
+    answer = arr[0]
+    for i in range(1, len(arr)):
+        answer = (answer * arr[i]) // gcd(answer, arr[i])
+    return answer

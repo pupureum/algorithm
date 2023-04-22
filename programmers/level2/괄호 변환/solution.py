78f4e967cb7b70e63c34d60b4614873790d3de11
pupureum <pureuml.dev@gmail.com>
2023-04-22 16:07:12 +0900

    feat: 괄호 변환

---
+++ programmers/level2/괄호 변환/solution.py
@@ -0,0 +1,44 @@
+def solution(p):
+    answer = ''
+    if p == '':
+        return answer
+    index = balanced_index(p)
+    u = p[:index + 1]
+    v = p[index + 1:]
+    if is_correct(u):
+        answer = u + solution(v)
+    else:
+        answer = '('
+        answer += solution(v)
+        answer += ')'
+        u = list(u[1:-1])
+        for i in range(len(u)):
+            if u[i] == '(':
+                u[i] = ')'
+            else:
+                u[i] = '('
+        answer += "".join(u)
+    return answer
+
+def balanced_index(p):
+    count = 0
+    for i in range(len(p)):
+        if p[i] == '(':
+            count += 1
+        else:
+            count -= 1
+        if count == 0:
+            return i
+
+def is_correct(p):
+    count = 0
+    for i in p:
+        if i == '(':
+            count += 1
+        else:
+            if count == 0:
+                return False
+            count -= 1
+    return True
+
+print(solution("()))((()"))
